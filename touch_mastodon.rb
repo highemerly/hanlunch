d7eac07da1f0fcaf7c295651b437a0a7e399e7b8
highemerly <high.emerly.ytfin@gmail.com>
2019-01-19 15:11:58 +0900

    [clean] delete files

--- touch_mastodon.rb
+++
@@ -1,6 +0,0 @@
-require 'mastodon'
-
-MASTODON_HOST = 'https://handon.club'
-
-mstdn = Mastodon::REST::Client.new(base_url: MASTODON_HOST, bearer_token: ENV['MASTODON_ACCESS_TOKEN'])
-mstdn.create_status('test')
--- vendor/ruby/2.3.0/gems/mastodon-api-1.1.0/lib/mastodon.rb
+++
@@ -1,4 +0,0 @@
-require 'addressable/uri'
-require 'mastodon/base'
-require 'mastodon/collection'
-require 'mastodon/rest/client'
